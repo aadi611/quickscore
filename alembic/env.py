f877be72642b8642f5856beaa2a131d8d2195884
Aadityan Gupta <89126304+aadi611@users.noreply.github.com>
2025-09-25 23:59:55 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from alembic import context
 import os
 import sys
 
-# Add the project root to Python path
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.models.database import Base
